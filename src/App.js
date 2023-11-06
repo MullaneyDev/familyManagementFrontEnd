c09f73a3b5c8d5571bb6444be3b8a5edaef5e39a
MullaneyDev <matt.mullaney@live.co.uk>
2023-11-06 17:13:07 +0000

    addition of LoggedIn/NotLoggedIn components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,26 @@
 import "./App.css";
+import {useState} from "react"
 import Footer from "./Components/layout/Footer/Footer";
 import Header from "./Components/layout/Header/Header";
+import NotLoggedIn from "./Components/Pages/NotLoggedIn/NotLoggedIn";
+import LoggedIn from "./Components/Pages/LoggedIn/LoggedIn";
 
 function App() {
+  const [loggedIn, setLoggedIn] = useState(false)
+
+  if (loggedIn) {
+    return (
+      <div className="App">
+        <Header />
+        <NotLoggedIn />
+        <Footer />
+      </div>
+    );
+  }
   return (
     <div className="App">
       <Header />
+      <LoggedIn />
       <Footer />
     </div>
   );
