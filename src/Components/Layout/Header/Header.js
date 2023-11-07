198a433c5de8bea2d9ce406035191556ecb02cb4
goleacornelia <goleacornelia725@gmail.com>
2023-11-07 12:36:37 +0000

    header

---
+++ src/Components/Layout/Header/Header.js
@@ -0,0 +1,21 @@
+import React from "react";
+import "./Header.css";
+
+const Header = ({ user, loggedIn }) => {
+  if (!loggedIn) {
+    return (
+      <div className="headerTop">
+        <h1>Boxed Off</h1>
+        <h3>register/sign in</h3>
+      </div>
+    );
+  } else {
+    return (
+      <div className="headerTop">
+        <h1>Boxed Off</h1>
+      </div>
+    );
+  }
+};
+
+export default Header;
