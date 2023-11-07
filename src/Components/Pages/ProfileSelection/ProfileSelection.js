a3622e3fb3b8db9749847167bee700735b8beea1
MullaneyDev <matt.mullaney@live.co.uk>
2023-11-07 12:11:03 +0000

    enhancement/ addition of parameters being passed through NotLoggedIn.js

--- src/Components/Pages/ProfileSelection/ProfileSelection.js
+++ src/Components/Pages/ProfileSelection/ProfileSelection.js
@@ -1,9 +1,14 @@
-import React from 'react'
+import React from "react";
+import "./ProfileSelection.css";
 
-const ProfileSelection = () => {
-  return (
-    <div>ProfileSelection</div>
-  )
-}
+const ProfileSelection = ({
+  setAdmin,
+  setUser,
+  setLoggedIn,
+  family,
+  members,
+}) => {
+  return <div>ProfileSelection</div>;
+};
 
-export default ProfileSelection
+export default ProfileSelection;
