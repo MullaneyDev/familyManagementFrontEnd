5fb5b590625094b7cc7e606c356bbff925758463
goleacornelia <goleacornelia725@gmail.com>
2023-11-08 09:33:39 +0000

    code rewrite as lost

--- src/Components/Pages/LoggedIn/LoggedIn.js
+++ src/Components/Pages/LoggedIn/LoggedIn.js
@@ -2,6 +2,32 @@ import React from "react";
 import "./LoggedIn.css";
 import TaskContainer from "../../Tasks/TaskContainer/TaskContainer";
 
-const LoggedIn = ({}) => {};
+const LoggedIn = ({
+  setLoggedIn,
+  setAdmin,
+  setFamily,
+  setUser,
+  family,
+  members,
+  setMembers,
+  avatar,
+  setAvatar,
+  user,
+}) => {
+  return (
+    <TaskContainer
+      setLoggedIn={setLoggedIn}
+      setAdmin={setAdmin}
+      setFamily={setFamily}
+      setUser={setUser}
+      family={family}
+      members={members}
+      setMembers={setMembers}
+      avatar={avatar}
+      setAvatar={setAvatar}
+      user={user}
+    />
+  );
+};
 
 export default LoggedIn;
