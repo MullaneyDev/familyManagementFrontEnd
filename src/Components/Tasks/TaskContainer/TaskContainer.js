e19e3d1d437da0444cdecb740e31015db1e31ee4
goleacornelia <goleacornelia725@gmail.com>
2023-11-09 09:42:27 +0000

    map thru temporary tasks

--- src/Components/Tasks/TaskContainer/TaskContainer.js
+++ src/Components/Tasks/TaskContainer/TaskContainer.js
@@ -1,8 +1,27 @@
 import React from "react";
 import "./TaskContainer.css";
+import { useState } from "react";
+import TaskCard from "../TaskCard/TaskCard";
 
 const TaskContainer = () => {
-  return <div class="TaskContainer">TaskContainer</div>;
+  const tempTasks = [
+    { id: 2, taskname: "Cleaning", points: 10 },
+    { id: 1, taskname: "Homework", points: 5 },
+    { id: 3, taskname: "Walk the dog", points: 15 },
+    { id: 5, taskname: "Cook dinner", points: 20 },
+    { id: 4, taskname: "Go to run", points: 12 },
+    { id: 6, taskname: "Tidy bedroom", points: 8 },
+    { id: 7, taskname: "Sort laundry", points: 14 },
+    { id: 9, taskname: "Make bed", points: 7 },
+    { id: 8, taskname: "Grocery shopping", points: 18 },
+  ];
+  return (
+    <div className="TaskContainer">
+      {tempTasks.map((task) => (
+        <TaskCard task={task} />
+      ))}
+    </div>
+  );
 };
 
 export default TaskContainer;
