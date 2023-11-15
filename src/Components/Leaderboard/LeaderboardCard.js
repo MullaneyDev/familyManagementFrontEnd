2f6628b6cee53ff9d5ac1a443c7f8ab7a230029b
MullaneyDev <matt.mullaney@live.co.uk>
2023-11-15 15:28:40 +0000

    addition of rewards tiers and new titles when points earned

--- src/Components/Leaderboard/LeaderboardCard.js
+++ src/Components/Leaderboard/LeaderboardCard.js
@@ -11,11 +11,21 @@ const LeaderboardCard = ({ member }) => {
         {member.totalPoints <= 0
           ? "Shrodingers Box"
           : member.totalPoints >= 0 && member.totalPoints < 100
-          ? "Boxer"
-          : member.totalPoints >= 100 && member.totalPoints < 250
-          ? "KickBoxer"
-          : member.totalPoints >= 250 && member.totalPoints < 500
-          ? "Hot Boxer"
+          ? "Cardboard Box"
+          : member.totalPoints >= 100 && member.totalPoints < 300
+          ? "Lunch Box"
+          : member.totalPoints >= 300 && member.totalPoints < 600
+          ? "Tool Box"
+          : member.totalPoints >= 600 && member.totalPoints < 700
+          ? "Jack in the Box"
+          : member.totalPoints >= 700 && member.totalPoints < 800
+          ? "Boom Box"
+          : member.totalPoints >= 800 && member.totalPoints < 900
+          ? "Kick Boxer"
+          : member.totalPoints >= 900 && member.totalPoints < 1000
+          ? "Box Office"
+          : member.totalPoints >= 1000 
+          ? "Pandora's Box"
           : "Who are you?"}
       </p>
       <p>{member.totalPoints}</p>
