dbb27c7b072237982711fe86db5317ece95b71e5
J Meyer <124314330+BinkSan3@users.noreply.github.com>
2023-11-16 15:43:26 +0000

    Function: reward tier working with css

--- src/Components/Rewards/Rewards.js
+++ src/Components/Rewards/Rewards.js
@@ -1,21 +1,32 @@
 import React from "react";
 import "./Rewards.css";
-import { getRewards, addFamilyReward, deleteReward } from "../../Utils";
+import {
+  getRewards,
+  addFamilyReward,
+  deleteReward,
+  editReward,
+} from "../../Utils";
 import { useEffect, useState } from "react";
 import RewardsCard from "./RewardsCard/RewardsCard";
+import Modal from "react-modal";
 
 const Rewards = ({ rewards, setRewards, admin }) => {
   const [tier, setTier] = useState();
   const [rewardPoints, setRewardPoints] = useState();
+  const [isOpen, setIsOpen] = useState(false);
+  const [displayed, setDisplayed] = useState({});
 
   useEffect(() => {
     const getData = async () => {
       const response = await getRewards();
-      console.log("TTTTTTT", response);
       setRewards(response.result);
     };
     getData();
   }, []);
+
+  const rewardsOrdered = rewards.sort(function (a, b) {
+    return b.rewardPoints - a.rewardPoints;
+  });
 
   const pointOptions = [
     { value: "", display: "Choose an Option" },
@@ -50,6 +61,16 @@ const Rewards = ({ rewards, setRewards, admin }) => {
     setRewards(newRewards);
   };
 
+  const handleEdit = async (e) => {
+    e.preventDefault();
+    await editReward(displayed.id, displayed.tier, displayed.rewardPoints);
+    const newRewards = [...rewards];
+    const index = newRewards.findIndex((reward) => reward.id === displayed.id);
+    newRewards[index] = displayed;
+    setRewards(newRewards);
+    setIsOpen(false);
+  };
+
   //useeffect to get
   //add edit and delete protected by admin
   // map through globalstate that will hold the get request
@@ -76,7 +97,13 @@ const Rewards = ({ rewards, setRewards, admin }) => {
           >
             {pointOptions.map((option) => (
               <option
-                disabled={option.disabled}
+                disabled={
+                  rewards.findIndex(
+                    (reward) => reward.rewardPoints === option.value
+                  ) === -1
+                    ? false
+                    : true
+                }
                 key={option.value}
                 value={option.value}
               >
@@ -91,29 +118,48 @@ const Rewards = ({ rewards, setRewards, admin }) => {
           />
         </form>
       </div>
-      {rewards.map((reward, index) => (
-        <RewardsCard
-          reward={reward}
-          key={index}
-          admin={admin}
-          handleDelete={handleDelete}
-        />
-      ))}
-      <div className="goldTier">
-        <h2>Choose the family day out</h2>
-        <h2>Points needed: 1000</h2>
-        <h2>Title Required: Pandora's Box</h2>
+      <div className="map-container-reward">
+        {rewardsOrdered.map((reward, index) => (
+          <RewardsCard
+            reward={reward}
+            key={index}
+            admin={admin}
+            handleDelete={handleDelete}
+            setIsOpen={setIsOpen}
+            setDisplayed={setDisplayed}
+          />
+        ))}
       </div>
-      <div className="silverTier">
-        <h2>Choose the takeaway</h2>
-        <h2>Points needed: 600</h2>
-        <h2>Title Required: Jack In The Box</h2>
-      </div>
-      <div className="bronzeTier">
-        <h2>Choose the family film</h2>
-        <h2>Points needed: 300</h2>
-        <h2>Title Required: Tool Box</h2>
-      </div>
+
+      <Modal
+        className="ModalStyle"
+        isOpen={isOpen}
+        onRequestClose={() => setIsOpen(false)}
+      >
+        <div className="editreward-container">
+          <h3 className="edit-reward-center">Add a family Reward</h3>
+          <form className="edit-reward" onSubmit={handleEdit}>
+            <h4>{displayed.rewardPoints}</h4>
+            <input
+              type="text"
+              name="tier"
+              className="input-reward"
+              placeholder="edit reward"
+              value={displayed.tier}
+              required="true"
+              onChange={(e) =>
+                setDisplayed({ ...displayed, tier: e.target.value })
+              }
+            />
+
+            <input
+              className="save-edit-btn-reward"
+              type="submit"
+              value="Edit Reward"
+            />
+          </form>
+        </div>
+      </Modal>
     </div>
   );
 };
--- src/Components/Rewards/RewardsCard/RewardsCard.js
+++ src/Components/Rewards/RewardsCard/RewardsCard.js
@@ -1,4 +1,9 @@
 import React from "react";
+import first from "../../../assets/first.svg";
+import second from "../../../assets/second.svg";
+import third from "../../../assets/third.svg";
+
+import "./RewardsCard.css";
 
 const RewardsCard = ({
   rewards,
@@ -8,19 +13,56 @@ const RewardsCard = ({
   key,
   index,
   handleDelete,
+  setIsOpen,
+  setDisplayed,
 }) => {
   return (
-    <div className="reward-container">
-      <p>{reward.tier}</p>
-      <p>{reward.rewardPoints}</p>
-      <button className="edit-reward-btn">Edit</button>
+    <div
+      className="reward-card"
+      bronze-theme={reward.rewardPoints === 300 && "bronze"}
+      silver-theme={reward.rewardPoints === 600 && "silver"}
+      gold-theme={reward.rewardPoints === 1000 && "gold"}
+    >
+      <div className="circle">
+        <h2 className="reward-content">{reward.tier}</h2>
+        <h2 className="reward-content">{reward.rewardPoints} Point Required</h2>
+        {reward.rewardPoints === 300 && (
+          <>
+            <h3 className="reward-content">Title Required: Tool Box</h3>
+            <img className="medal" src={third} alt="3rd place medal" />
+          </>
+        )}
+        {reward.rewardPoints === 600 && (
+          <>
+            <h3 className="reward-content">Title Required: Lunch Box</h3>
+            <img className="medal" src={second} alt="2nd place medal" />
+          </>
+        )}
+        {reward.rewardPoints === 1000 && (
+          <>
+            <h3 className="reward-content">Title Required: Pandora's Box</h3>
+            <img className="medal" src={first} alt="1st place medal" />
+          </>
+        )}
+        <div className="reward-buttons">
+          <button
+            className="edit-btn-reward"
+            onClick={() => {
+              setIsOpen(true);
+              setDisplayed(reward);
+            }}
+          >
+            Edit
+          </button>
 
-      <button
-        className="delete-btn-reward"
-        onClick={(e) => handleDelete(e, reward.id)}
-      >
-        Delete
-      </button>
+          <button
+            className="delete-btn-reward"
+            onClick={(e) => handleDelete(e, reward.id)}
+          >
+            Delete
+          </button>
+        </div>
+      </div>
     </div>
   );
 };
